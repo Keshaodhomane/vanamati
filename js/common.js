9888687232d4eeb3163e438f7c5ba2291563dec7
Keshao Dhomane <keshaodhomane@gmail.com>
2017-06-20 12:50:40 +0530

    Initial commit

---
+++ js/common.js
@@ -0,0 +1,158 @@
+$(document).ready(function () {
+
+    // Slider Options
+    $(".Modern-Slider").slick({
+        autoplay: true,
+        autoplaySpeed: 5000,
+        speed: 600,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        pauseOnHover: false,
+        dots: true,
+        pauseOnDotsHover: true,
+        cssEase: 'linear',
+        fade: true,
+        draggable: false,
+        prevArrow: '<button class="PrevArrow"> <span class="Thumbnail"></span></button>',
+        nextArrow: '<button class="NextArrow"> <span class="Thumbnail"></span></button>',
+        // Custom Dots With Thumbnails Tool Tip
+        customPaging: function (slider, i) {
+            var thumbnail = $(slider.$slides[i]).data('thumbnail');
+            return '<a href="javascript:void(0)"><img src="' + thumbnail + '"></a>';
+        },
+    });
+
+    // Arrows Thumbnails Trick
+    var PrevThumbinalFirst = $(".Modern-Slider .item:last-child").children(".img-fill").children("img").attr("src");
+    $(".Modern-Slider .PrevArrow .Thumbnail").css({
+        "background-image": "url(" + PrevThumbinalFirst + ")"
+    })
+    var NextThumbinalFirst = $(".Modern-Slider .item:first-child").next().children(".img-fill").children("img").attr("src");
+    $(".Modern-Slider .NextArrow .Thumbnail").css({
+        "background-image": "url(" + NextThumbinalFirst + ")"
+    })
+    $(".Modern-Slider").on('afterChange', function (event, slick, currentSlide) {
+        var PrevThumbinal = $(".Modern-Slider .item.slick-active").prev(".item").children(".img-fill").children("img").attr("src");
+        $(".Modern-Slider .PrevArrow .Thumbnail").css({
+            "background-image": "url(" + PrevThumbinal + ")"
+        })
+        var NextThumbinal = $(".Modern-Slider .item.slick-active").next(".item").children(".img-fill").children("img").attr("src");
+        $(".Modern-Slider .NextArrow .Thumbnail").css({
+            "background-image": "url(" + NextThumbinal + ")"
+        })
+    });
+
+
+
+
+
+
+
+    function switchStyleSheets(theme) {
+        $('link[title="theme"]').attr('href', theme);
+    }
+
+    $(document).ready(function () {
+
+
+        $('#black').click(function (event) {
+            event.preventDefault();
+            switchStyleSheets(
+                'css/black.css'
+            );
+        });
+
+
+        $('#default').click(function (event) {
+            event.preventDefault();
+            switchStyleSheets(
+                'css/default.css'
+            );
+        });
+
+
+
+         $('#blue').click(function(event){
+        event.preventDefault();
+        switchStyleSheets(
+            'css/blue.css'        
+        );
+    });
+
+
+
+
+        $('#green').click(function (event) {
+            event.preventDefault();
+            switchStyleSheets(
+                'css/green.css'
+            );
+        });
+
+    });
+
+
+
+
+
+
+    /* this changes font size of */
+
+    var originalSize = $('div').css('font-size');
+    // reset
+    $(".resetMe").click(function () {
+        $('div').css('font-size', originalSize);
+
+    });
+
+    // Increase Font Size
+    $(".increase").click(function () {
+        var currentSize = $('div').css('font-size');
+        var currentSize = parseFloat(currentSize) * 1.2;
+        $('div').css('font-size', currentSize);
+
+        return false;
+    });
+
+    // Decrease Font Size
+    $(".decrease").click(function () {
+        var currentFontSize = $('div').css('font-size');
+        var currentSize = $('div').css('font-size');
+        var currentSize = parseFloat(currentSize) * 0.8;
+        $('div').css('font-size', currentSize);
+
+        return false;
+    });
+
+
+
+
+
+
+
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay:true,
+        autoplayTimeout:6000,
+        smartSpeed:800
+        
+        //animateOut: 'fadeOut'
+    });
+    
+    
+    
+    
+    var offsetHeight = document.getElementById('myDiv').offsetHeight;
+    
+    console.log(offsetHeight);
+    
+    
+    $("#someElement").css({ 'height': offsetHeight + "px" });
+
+    
+    
+
+   
+})
